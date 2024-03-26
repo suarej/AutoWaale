dd17c5f98917df4bb533943e651ba1e76c9d6977
surajshelke.24@gmail.com <surajshelke.24@gmail.com>
2024-03-26 07:30:24 +0530

    apk still not right-loader animation added

--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
-import { StyleSheet, View, Text } from "react-native";
-import { useEffect, useState, useCallback } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { FIREBASE_AUTH } from "../firebaseConfig";
 import Loginform from "./loginForm";
 import Dashboard from "./dashboard";
-import Entypo from '@expo/vector-icons/Entypo';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
+import LottieView from "lottie-react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 // SplashScreen.preventAutoHideAsync();
 
 export default function Page() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [user, setUser] = useState();
+  const animation = useRef(null);
 
   useEffect(() => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
@@ -19,12 +22,62 @@ export default function Page() {
     });
   }, [user]);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <View style={styles.animationContainer}>
+          <LottieView
+            autoPlay
+            ref={animation}
+            style={{
+              width: 200,
+              height: 200,
+              backgroundColor: "#eee",
+            }}
+            // Find more Lottie files at https://lottiefiles.com/featured
+            source={require("../assets/loaderAnimation.json")}
+          />
+        </View>
+    );
+  }
+
   return (
-      <View style={styles.container}>
-        {user ? <Dashboard user={user} /> : <Loginform />}
-      </View>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      {user ? <Dashboard user={user} /> : <Loginform />}
+    </View>
   );
 }
+
+// animation.current?.reset();
+// animation.current?.play();
 
 const styles = StyleSheet.create({
   container: {
@@ -35,5 +88,14 @@ const styles = StyleSheet.create({
     resizeMode: "center",
     justifyContent: "center",
     opacity: 0.9,
+  },
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
   },
 });
